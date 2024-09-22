204b23b24f13a8e1468461c004ca9896981cb1a6
sebe <sebe2k04@gmail.com>
2024-09-22 11:11:34 +0530

    node js --projectController --delete added

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -105,6 +105,17 @@ const updateProject = async (req, res) => {
   }
 };
 
-
+const deleteProduct = async (req, res) => {
+    try {
+      const project = await Projects.findByIdAndDelete({ _id: req.params.id });
+      console.log(project);
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+      res.json({ message: "Project removed" });
+    } catch (error) {
+      res.status(500).json({ message: "Server Error" });
+    }
+  };
 
 
