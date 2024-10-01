3f4cc3369af4103dea9ecb0c63e727e25b5eb05a
sebe <sebe2k04@gmail.com>
2024-10-01 22:19:04 +0530

    ui change

--- client/src/components/HireIcon.jsx
+++ client/src/components/HireIcon.jsx
@@ -1,21 +1,34 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const HireIcon = () => {
+  const path = usePathname();
   return (
     <div>
-      <div className="fixed z-[100] lg:bottom-[30px] bottom-[20px] right-[10px]  lg:right-[50px] lg:scale-75 scale-50">
-        <img src="/crtxt.png" alt="" className="w-[145px] animate-round relative z-[50]" />
+      {path.startsWith("/admin") ? (
+        ""
+      ) : (
+        <div className="fixed z-[100] lg:bottom-[30px] bottom-[20px] right-[10px]  lg:right-[50px] lg:scale-75 scale-50">
+          <img
+            src="/crtxt.png"
+            alt=""
+            className="w-[145px] animate-round relative z-[50]"
+          />
 
-        <Link href={"/#contact"} className="flex justify-center relative z-[60]">
-          <div className="w-[90px] h-[90px] border mt-[-117px] flex justify-center items-center bg-black text-white hover: rounded-full">
-            <div className="text-center">
-              <h1 className="hello text-2xl">Hire </h1>
-              <h1 className="hello text-2xl">Me </h1>
+          <Link
+            href={"/#contact"}
+            className="flex justify-center relative z-[60]"
+          >
+            <div className="w-[90px] h-[90px] border mt-[-117px] flex justify-center items-center bg-black text-white hover: rounded-full">
+              <div className="text-center">
+                <h1 className="hello text-2xl">Hire </h1>
+                <h1 className="hello text-2xl">Me </h1>
+              </div>
             </div>
-          </div>
-        </Link>
-      </div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
