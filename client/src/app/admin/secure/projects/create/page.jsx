01df6e2034db046e8d5cac1273cf9cefb9c34d51
sebe <sebe2k04@gmail.com>
2024-10-02 10:52:59 +0530

    next js - ui change

--- client/src/app/admin/secure/projects/create/page.jsx
+++ client/src/app/admin/secure/projects/create/page.jsx
@@ -154,7 +154,6 @@ export default function Page() {
             <label className=" text-sm font-medium text-gray-700">Skills</label>
             <input
               type="text"
-              required
               value={tech}
               onChange={(e) => setTech(e.target.value)}
               className=" w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-gray-600 sm:text-sm"
