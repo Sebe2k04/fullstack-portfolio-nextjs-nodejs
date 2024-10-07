6182768b8418caa1cbef0d2cc36eb3068ea6d150
sebe <sebe2k04@gmail.com>
2024-10-07 20:54:48 +0530

    animated

--- client/src/components/Home/Skills.jsx
+++ client/src/components/Home/Skills.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { FaHtml5 } from "react-icons/fa6";
 import { IoIosArrowDown, IoLogoCss3 } from "react-icons/io";
@@ -17,8 +18,7 @@ import { IoLogoFirebase } from "react-icons/io5";
 import { RiNextjsFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
 import { FaGitAlt } from "react-icons/fa";
-
-
+import { motion } from "framer-motion";
 
 const skills = [
   {
@@ -101,7 +101,7 @@ const skills = [
     icon: <FaGithub />,
     name: "Github",
   },
-  
+
   {
     color: "text-orange-400",
     icon: <FaGitAlt />,
@@ -113,35 +113,58 @@ const Skills = () => {
   return (
     <div className="lg:px-20 px-8 py-10">
       <div className="">
-        <h1 className="text-3xl font-semibold">
+        <motion.h1
+          initial={{ opacity: 0, scale: 0.4, x: 100 }}
+          whileInView={{ opacity: 1, scale: 1, x: 0 }}
+          transition={{ type: "spring", stiffness: 200, damping: 50 }}
+          viewport={{ once: true }}
+          className="text-3xl font-semibold"
+        >
           <span className="text-4xl font-normal hello text-orange-400">
             Skills
           </span>{" "}
           that I know
-        </h1>
+        </motion.h1>
         <div className="flex justify-center md:px-10">
-          <h5 className="py-5 md:text-center text-justify">
+          <motion.h5
+            initial={{ opacity: 0, scale: 0.4, y: 100 }}
+            whileInView={{ opacity: 1, scale: 1, y: 0 }}
+            transition={{ type: "spring", stiffness: 200, damping: 50 }}
+            viewport={{ once: true }}
+            className="py-5 md:text-center text-justify"
+          >
             I am an enthusiastic, self-motivated, reliable, responsible and hard
             working person. I am a good team worker and adaptable to all
             challenging situations. I am able to work well both in a team
             environment as well as using own initiative. I am able to work well
             under pressure and adhere to strict deadlines.
-          </h5>
+          </motion.h5>
         </div>{" "}
       </div>
       <div className="flex justify-center">
-        <div className="grid grid-cols-3 text-center md:flex flex-wrap justify-center items-center gap-10 py-8 max-w-[900px]">
+        <div className="grid grid-cols-3 text-center md:flex flex-wrap justify-center items-center gap-5 lg:gap-10 py-8 max-w-[900px]">
           {skills.map((skill, i) => {
             return (
-              <div key={i} className={`${skill.color}`}>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.4, y: 20 }}
+                whileInView={{ opacity: 1, scale: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 200,
+                  damping: 50,
+                  delay: i / 10,
+                }}
+                viewport={{ once: true }}
+                key={i}
+                className={`${skill.color}`}
+              >
                 <div className="w-fit h-fit border border-b-4 text-3xl md:text-5xl rounded-full p-5 ">
                   {skill.icon}
                 </div>
                 <h1 className="text-center text-sm pt-2">{skill.name}</h1>
-              </div>
+              </motion.div>
             );
           })}
-         
         </div>
       </div>
     </div>
