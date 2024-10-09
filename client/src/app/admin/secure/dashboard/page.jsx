05545ddcdcebd2850d77e74ec875b62a6d299e54
sebe <sebe2k04@gmail.com>
2024-10-09 06:24:53 +0530

    ui changes

--- client/src/app/admin/secure/dashboard/page.jsx
+++ client/src/app/admin/secure/dashboard/page.jsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <div className="flex justify-center items-center w-full h-full">
+        <div className="flex justify-center items-center w-full h-[80vh]">
             <h1 className="text-2xl font-semibold">Coming Soon...</h1>
         </div>
     );
