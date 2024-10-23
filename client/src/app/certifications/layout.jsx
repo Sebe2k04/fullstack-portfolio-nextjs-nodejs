045706c99f3c363881ac13f3f38c11527e430b93
sebe <sebe2k04@gmail.com>
2024-10-23 21:04:14 +0530

    seo update

---
+++ client/src/app/certifications/layout.jsx
@@ -0,0 +1,42 @@
+
+
+export const metadata = {
+    title: "Certifications",
+    description:
+      "Explore my certifications to showcase my technical capability and to ensure my expertise to endulge on real world projects",
+    keywords:
+      "sebe , sebe s , sebe saravanan , sebe2k04 , developer portfolio ,trending portfolio, axolotron , dnyx, software engineer , web developer , portfolio , fullstack portfolio , nandha college of technology , nandha , cse , dev , developer , mongodb , express, nodejs , reactjs , nextjs, genrio , sebe , cb , website , web, dev , engineer , techie , nct , fresher , 2025 , erode ",
+    url: process.env.NEXT_PUBLIC_CLIENT_URL,
+    canonical: process.env.NEXT_PUBLIC_CLIENT_URL,
+    authors: [
+      { name: "Sebe S", url: process.env.NEXT_PUBLIC_CLIENT_URL }
+    ],
+    openGraph: {
+      title: "Certifications",
+      description:
+       "Explore my certifications to showcase my technical capability and to ensure my expertise to endulge on real world projects",
+      url: process.env.NEXT_PUBLIC_CLIENT_URL,
+      siteName: "Sebe S - Portfolio",
+      images: [
+        {
+          url: "https://res.cloudinary.com/ded1o1e26/image/upload/v1726405493/profile_ioufgc.png",
+          width: 1200,
+          height: 640,
+          alt: "Sebe S | Software Engineer | Web Developer | Portfolio",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    
+    viewport: "width=device-width, initial-scale=1.0",
+    robots: "index, follow",
+  };
+
+export default function Layout({children}) {
+    return (
+        <section>
+            {children}
+        </section>
+    );
+}
--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -24,9 +24,36 @@ import Script from "next/script";
 // });
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
-  title: "Sebe S",
+  title: "Sebe S | Software Engineer | Portfolio",
   description:
     "a Passionate web developer and Tech Enthusiast who interest on coding and eager to learn develop Web Applications and Software using Javascript programming language . I'm a fresher and I can adapt various tech environments and perform collaborative works and scalable projects",
+  keywords:
+    "sebe , sebe s , sebe saravanan , sebe2k04 , developer portfolio ,trending portfolio, axolotron , dnyx, software engineer , web developer , portfolio , fullstack portfolio , nandha college of technology , nandha , cse , dev , developer , mongodb , express, nodejs , reactjs , nextjs, genrio , sebe , cb , website , web, dev , engineer , techie , nct , fresher , 2025 , erode ",
+  url: process.env.NEXT_PUBLIC_CLIENT_URL,
+  canonical: process.env.NEXT_PUBLIC_CLIENT_URL,
+  authors: [
+    { name: "Sebe S", url: process.env.NEXT_PUBLIC_CLIENT_URL }
+  ],
+  openGraph: {
+    title: "Sebe S | Software Engineer | Portfolio",
+    description:
+      "a Passionate web developer and Tech Enthusiast who interest on coding and eager to learn develop Web Applications and Software using Javascript programming language . I'm a fresher and I can adapt various tech environments and perform collaborative works and scalable projects",
+    url: process.env.NEXT_PUBLIC_CLIENT_URL,
+    siteName: "Sebe S - Portfolio",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ded1o1e26/image/upload/v1726405493/profile_ioufgc.png",
+        width: 1200,
+        height: 640,
+        alt: "Sebe S | Software Engineer | Web Developer | Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
 };
 
 export default function RootLayout({ children }) {
@@ -34,8 +61,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="/sebeico.png" type="image/x-icon" />
-         {/* Google Analytics */}
-         <Script
+        {/* Google Analytics */}
+        <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=G-TWWC4KFJPK`}
         />
@@ -55,14 +82,14 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="inter">
-        <SEO
+        {/* <SEO
           title="Sebe S"
           description="a Passionate web developer and Tech Enthusiast who interest on coding and eager to learn develop Web Applications and Software using Javascript programming language . I'm a fresher and I can adapt various tech environments and perform collaborative works and scalable projects , this project is one of my full-stack projects built with Next.js, Node.js, Tailwind CSS, MongoDB, and more."
           keywords="Portfolio, portfolio, sebpage , sebe2k04, trend, developer , nandha college of technology, sebe saravanan , sebe s, Next.js, Node.js, Full-Stack, Web Development, MongoDB, Tailwind CSS , sebe, sebe2k04,full stack project , ecommerce , genrio"
           image="/profile.png"
           author="sebe2k04"
           url={process.env.NEXT_PUBLIC_CLIENT_URL}
-        />
+        /> */}
         {/* <FixedBtn /> */}
         <GlobalProvider>
           <NavHandler />
